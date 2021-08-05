a9508107eeeebb559a4a76fd82d74c790e5894a9
EstenoesJavi <cristiancamilomunozarango@gmail.com>
2021-08-04 23:51:00 -0500

    Firts commit

---
+++ src/main/java/com/pruebas/tareasProgramadas/TareasProgramadasApplication.java
@@ -0,0 +1,13 @@
+package com.pruebas.tareasProgramadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareasProgramadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareasProgramadasApplication.class, args);
+	}
+
+}
